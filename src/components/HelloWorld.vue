aed20f8160f4e0dc9c970f0bd3990b74db80b3a4
Bezruchko <mnt_13@mail.ru>
2023-07-10 15:01:02 +0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import sourceData from '@/data.json'
+
+// console.log(sourceData)
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+})
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with Awersome shit!
+    </h3>
+    <p></p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
